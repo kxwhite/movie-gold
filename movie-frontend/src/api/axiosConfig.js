4aa54a588d677b15059e943cb0e9c94298fbd68b
Keon White <keon-1@hotmail.co.uk>
2024-08-10 13:27:13 +0100

    Added PORT as a fallback

--- movie-frontend/src/api/axiosConfig.js
+++ movie-frontend/src/api/axiosConfig.js
@@ -1,14 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.PORT || "http://localhost:8080",
   headers: { "skip-browser-warning": "true" },
 });
-// export default axios.create({
-//   baseURL: "https://c1a6-2a01-4b00-8321-200-710b-681e-f1bd-aca7.ngrok-free.app",
-//   headers: { "ngrok-skip-browser-warning": "true" },
-// });
-// export default axios.create({
-//   baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
-//   headers: {"ngrok-skip-browser-warning": "true"}
-// });
