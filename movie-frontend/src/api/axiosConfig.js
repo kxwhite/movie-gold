7aa45815c65b68ee863362d2a3d68fa04c46b971
Keon White <keon-1@hotmail.co.uk>
2024-08-17 16:55:07 +0100

    Added backend url, adjusted config and added a development and production file for the backend url.

--- movie-frontend/src/api/axiosConfig.js
+++ movie-frontend/src/api/axiosConfig.js
@@ -1,14 +1,6 @@
-// import axios from "axios";
-
-// export default axios.create({
-//   baseURL: "http://localhost:8080",
-//   headers: { "skip-browser-warning": "true" },
-// });
-
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:8080";
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export default axios.create({
   baseURL: API_BASE_URL,
