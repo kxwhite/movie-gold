a6c1995e572ca8350fd88d7d285311e0b634e0e8
Keon White <keon-1@hotmail.co.uk>
2024-08-17 11:54:10 +0100

    Adding production vs deployment to axios. Added all spring paths for visibility.

--- movie-frontend/src/api/axiosConfig.js
+++ movie-frontend/src/api/axiosConfig.js
@@ -1,6 +1,16 @@
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL: "http://localhost:8080",
+//   headers: { "skip-browser-warning": "true" },
+// });
+
 import axios from "axios";
 
+const API_BASE_URL =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:8080";
+
 export default axios.create({
-  baseURL: process.env.PORT || "http://localhost:8080",
+  baseURL: API_BASE_URL,
   headers: { "skip-browser-warning": "true" },
 });
